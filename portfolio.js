b41123788bd78c285dadee3842096a173b8c6258
mj <mjmartinsu@yahoo.es>
2024-11-07 13:21:16 +0100

    Clase de programación en portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,56 @@
+'use strict'
+
+// sentencias
+
+// expresiones
+
+// variables y datos
+
+
 console.log("nuevo js para portfolio")
 
 console.log("sabor a jamón")
+
+console.log(22, 'Pepe', true)
+console.log (22, typeof 22)
+console.log ('Pepe', typeof 'Pepe')
+consoile.log (true, typeof true)
+
+// se declaran con var - let - const
+
+let user
+let age
+let isStudent
+
+// asignacion
+
+user = 'Pepe'
+age = 22
+isStudent = true
+
+
+// CONST
+
+const course = `Java Script`
+
+// Da un error
+// course = 'Typescript'
+
+let emotionalState = 'sad'
+console.log (emotionalState)
+
+let score = 5 + 4
+// es igual que si yo escribo esto
+score = 9
+
+let newScore = 3 * score
+let otherScore = newScore
+newScore = 54
+
+console.log(newScore) // espero el valor 54
+console.log(otherScore) // espero 27
+
+console.log (newScore > otherScore) // espero true 
+console.log (newScore === otherscore) // espero false 
+
+console.log(2 === '2')  // false
