79f6fe6bf017b00e2b180e34907b4a14c2d118e3
mj <mjmartinsu@yahoo.es>
2024-11-08 13:32:25 +0100

    he añadido index2.html y js

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ console.log('Index2 loaded')
 function greeting() {
     const question = '¿Cómo te llamas?'
     const name = prompt (question) 
-    if(name !== null) {
+    if(name !== null || name === ``) {
         const response = `Hola, ${name}, qué tal estás`
         alert(response)
     }
