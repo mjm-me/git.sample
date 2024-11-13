b15adb72871d4125db7c04222a9688c33c268835
mj <mjmartinsu@yahoo.es>
2024-11-13 13:53:32 +0100

    añado ejercicios de dificultad media

---
+++ medios.js
@@ -0,0 +1,65 @@
+// // 1. Crea una función que elimine el primer y último carácter de un string recibido por parámetros.
+// las funciones son objetos y se pueden escribir function nmbrefunción()
+
+const removeLastFirst = function (value = "") {
+    let result = "";
+    for (let i = 1; i < value.length - 1; i++) {
+        result += value[i];
+    }
+    return result;
+
+    //const r = value.slice(1, value.length -1); //hace lo mismo
+};
+console.log(removeLastFirst("Patata"));
+
+// 2. Escribe una función que reciba una palabra y revise si es un palíndromo.
+
+// 3. Crea una función que cuente las vocales que contiene una palabra dada por parámetros.
+const countVocals = function (value = "") {
+    const lowerValue = value;
+    let accumulator = 0;
+    const vocals = "aeiouáéíóúü";
+    for (let i = 0; i < lowerValue.length; i++) {
+        const item = lowerValue[i]; //es cada una de las letras en cada vuelta
+        accumulator += vocals.includes(item); //la coerción ya me suma el 1 y 0 los strings (vocals.) ya iteran
+    }
+    return accumulator;
+};
+console.log(countVocals("El murciélago es verde")); //me devuelve 9 que son las 9 vocales
+
+const countVocals1 = function (value = "") {
+    let accumulator = 0;
+    const vocals = ["a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú", "ü"]; //sirve para el ejemplo del if
+    for (let i = 0; i < value.length; i++) {
+        const item = value[i].toLocaleLowerCase(); //es cada una de las letras en cada vuelta
+        if (vocals.includes(item)) {
+            accumulator++;
+        }
+    }
+    return accumulator;
+};
+console.log(countVocals1("Murciélago")); //me devuelve 5 que son las 5 vocales
+
+// 4. Crea una función que verifique si una cadena de texto recibida por parámetros es un pangrama (contiene todas las letras del abecedario).
+
+// 5. Escribe una función que compruebe si una cadena de texto contiene todas las vocales.
+
+// 6. Crea una función que realice una cuenta atrás desde un número recibido por parámetros.
+
+// 7. Escribe una función que reciba por parámetros el año de nacimiento, y calcule la edad de la persona.
+
+// 8. Crea una función que reciba la edad de una persona por parámetros y verifique si es mayor de edad. Imprime por consola un string con el resultado.
+
+// 9. Crea una función que simule el lanzamiento de un dado e imprime por consola el resultado cada vez que se ejecuta.
+
+// 10. Crea una función que reciba un año por parámetros y compruebe e imprima por consola si el año es bisiesto o no.
+
+// 11. Escribe una función que simula el juego piedra, papel y tijera. Recibirá como parámetro una opción (piedra, papel o tijera) en forma de string. La máquina, elegirá automáticamente una opción aleatoria. Imprime por consola ambas elecciones y en caso de ganar el jugador un mensaje de victoria, y en caso de perder uno de derrota.
+
+// 12. La serie de Fibonacci es un problema matemático que realiza la suma de los dos números anteriores para generar el siguiente. Crea una función que imprima por consola la serie de Fibonacci hasta un número introducido por el usuario. El usuario debe ser preguntado por este número al iniciar la aplicación.
+
+// 13. Escribe una función generadora de nombres de usuario aleatorios, a partir de dos grupos de palabras dadas. Estos grupos de palabras pueden estar agrupados en arrays. (nombres=['Hugo', 'Luis'], apellidos=['Duro', 'Fabiano']). Retorna un nombre de usuario aleatorio con nombre, apellido y un número aleatorio del 1 al 100. (Por ejemplo -> 'Pepe Pérez 87'.)
+
+// 14. Crea una función calculadora de propinas. Debe recibir el total de la cuenta y el porcentaje de propina deseado, con ello deberá calcular e imprimir por consola la cuenta, la propina que corresponde a la cuenta introducida, y el total a pagar. Redondea a dos decimales.
+
+// 15. Escribe una función que calcule el descuento aplicado a un precio. La función recibirá el precio y el descuento del artículo en venta, con ellos deberá calcular e imprimir por consola el precio, el descuento y el total del precio una vez aplicado el descuento. Redondea a dos decimales.
